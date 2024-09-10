71ff672e261f140e15373bc0dd76fe284f522178
yatish <ydipankar@gmail.com>
2024-09-09 23:39:51 -0300

    changes

---
+++ src/services/interceptor.jsx
@@ -0,0 +1,11 @@
+import apiClient from "./apiClient";
+import { getAuthToken, refreshAuthToken } from "./authService";
+
+apiClient.interceptors.request.use((config) => {
+	if (config.useAuth) {
+		const token = getAuthToken();
+		if (token) {
+			config.headers.Authorization = `Bearer ${token}`;
+		}
+	}
+});
