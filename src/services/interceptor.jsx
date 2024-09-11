a69b1d122c3741a676bb0b0005dc2a53601690aa
yatish <ydipankar@gmail.com>
2024-09-10 23:20:48 -0300

    completed interceptor

--- src/services/interceptor.jsx
+++ src/services/interceptor.jsx
@@ -1,11 +1,15 @@
 import apiClient from "./apiClient";
 import { getAuthToken, refreshAuthToken } from "./authService";
 
-apiClient.interceptors.request.use((config) => {
-	if (config.useAuth) {
-		const token = getAuthToken();
-		if (token) {
-			config.headers.Authorization = `Bearer ${token}`;
+apiClient.interceptors.request.use(
+	(config) => {
+		if (config.useAuth) {
+			const token = getAuthToken();
+			if (token) {
+				config.headers.Authorization = `Bearer ${token}`;
+			}
 		}
-	}
-});
+		return config;
+	},
+	(error) => Promise.reject(error)
+);
