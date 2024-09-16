b47cbb8aa36330a8124834521f4d369e922d2a0b
yatish <ydipankar@gmail.com>
2024-09-16 00:37:18 -0300

    addedinpur error

--- src/components/Common/FormInput.jsx
+++ src/components/Common/FormInput.jsx
@@ -1,19 +1,39 @@
 import React from "react";
 
-const FormInput = ({ label, type, value, onChange, placeholder, name }) => {
+const FormInput = ({
+	label,
+	type,
+	value,
+	onChange,
+	placeholder,
+	name,
+	error,
+}) => {
 	return (
-		<div>
-			<label className="block text-charcoalGray text-sm font-medium mb-1">
+		<div className="mb-4">
+			{/* Label */}
+			<label className="block text-gray-400 text-xs font-medium mb-2">
 				{label}
 			</label>
+
+			{/* Input Field */}
 			<input
 				type={type}
 				value={value}
 				name={name}
 				onChange={onChange}
 				placeholder={placeholder}
-				className="w-full p-3 border border-gray-300 rounded-md shadow-sm focus:outline-none"
-			></input>
+				className={`w-full p-3 border rounded-md shadow-sm transition duration-150 ease-in-out
+          ${
+						error
+							? "border-red-500 focus:border-red-500 focus:ring-red-500"
+							: "border-gray-300 focus:border-blue-500 focus:ring-blue-500"
+					}
+        `}
+			/>
+
+			{/* Error Message */}
+			{error && <p className="text-xs text-red-500 mt-1">{error}</p>}
 		</div>
 	);
 };
